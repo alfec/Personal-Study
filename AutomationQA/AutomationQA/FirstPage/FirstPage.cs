60d473606f84c24d659d5c5210adfb4b7cfb80b3
André Luiz <xester22@hotmail.com>
2019-03-11 10:48:34 -0300

    Primeiro codigo do Projeto

---
+++ AutomationQA/AutomationQA/FirstPage/FirstPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FirstPage
+{
+    class Program
+    {
+        static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.linkedin.com/in/alcarvalho3-1994/");
+            Thread.Sleep(3000);
+
+            driver.Quit();
+
+        }
+    }
+}
